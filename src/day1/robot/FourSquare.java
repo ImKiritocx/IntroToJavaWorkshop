9364cf287f1afe26431cbd244eedfab6a305abf8
league-workshopper-3 <workshop@Mac-mini.local>
2015-07-27 18:01:40 -0700

    Learned Java

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -2,32 +2,44 @@ package day1.robot;
 
 import javax.swing.JOptionPane;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class FourSquare {
 	
-	// 1. Create a new Robot
 	
+	Robot diego = new Robot();
 
 	void go() {
 		// 3. Make the robot move as fast as possible
-
+		diego.setSpeed(10);
 		// 4. Set the pen width to 5
-
+		diego.penDown();
+		
 		// 5. Do steps #6 to #7 four times...
-
+		for (int i = 0; i < 4; i++) {
+			
+		
 			// 6. Set the pen color to random
-	
+			diego.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
+			
 			// 7. Turn the robot 90 degrees to the right
-
+			diego.turn(90);
+		}
 	}
 
 	/* 2. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
+		//JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		for (int i = 0; i < 4; i++) {
+			
 		
-		
+		diego.move(200);
+		diego.turn(90);
+		diego.setPenWidth(i);
+		diego.setRandomPenColor();
+		}
 	}
 
 	public static void main(String[] args) {
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,5 +1,7 @@
 package day1.robot;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -7,8 +9,33 @@ public class MyFirstJavaProgram {
 		
 		// START HERE
 
-		new Robot();
-		
-		
+
+Robot kirito = new Robot();
+kirito.penDown();
+kirito.setPenWidth(50);
+kirito.setRandomPenColor();
+kirito.setSpeed(500);
+for (int i = 0; i < 4; i++) {
+	kirito.move(200);
+	kirito.turn(90);
+	kirito.setRandomPenColor();
+}
+kirito.turn(180);
+for (int i = 0; i < 4; i++) {
+	kirito.move(200);
+	kirito.turn(270);
+	kirito.setRandomPenColor();
+}
+for (int i = 0; i < 4; i++) {
+	kirito.move(200);
+	kirito.turn(90);
+	kirito.setRandomPenColor();
+}
+kirito.turn(180);
+for (int i = 0; i < 4; i++) {
+	kirito.move(200);
+	kirito.turn(270);
+	kirito.setRandomPenColor();
+}
 	}
 }
