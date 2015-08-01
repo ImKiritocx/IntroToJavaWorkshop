da0ba362797c04037aee7cefef77f68a568c9b5c
league-workshopper-8 <workshop@WTS1.local>
2015-07-31 17:36:42 -0700

    scarymap

--- src/day1/robot/StarShow.java
+++ src/day1/robot/StarShow.java
@@ -13,35 +13,38 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+			//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+		robot.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
 		int x = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-		int y = 950;
+		int y = 600;
 		// 8. Make a variable to hold the star size and set it to 25
-		int starsize = 25;
+		int starSize = 25;
 		// 12. Repeat the steps #19 to #18, 30 times
-
+		for (int i = 0; i < 30; i++) {
+			
+		
 			// 19. Set the pen width to i 
 	
 			// 10. Set the X position of the robot to your X variable
-	
+			robot.setX(x);
 			// 11. Set the Y position of the robot to your Y variable
-	
+			robot.setY(y);
 			// 9. Call the drawStar() method with your star size variable
-	
+			drawStar(starSize);
 			// 14. Increase the X position by star size. See Figure 2.
-	
+			x=x+starSize;
 			// 15. decrease the Y position by star size. See Figure 3.
-	
+			y=y-starSize;
 			// 16. Increase the star size by 20
-	
+			starSize=starSize+20;
 			// 17. Turn the robot 12 degrees
-	
-			// 18. Make each star a different random color like in Figure 4. 
-
+			robot.turn(12);
+			// 18. Make each star a different random color like in Figure 4.
+			robot.setRandomPenColor();
+		}
 	}
 
 	private void drawStar(int starSize) {
